2a57204cc6e9ecb70c4e063b2bbebe3e21e677e6
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-21 22:30:30 +1000

    feat: finished search and update methods

--- src/vault.py
+++ src/vault.py
@@ -9,6 +9,17 @@ def addPassword(website, email_username, password):
 
     print("password successfully added")
 
+def search(searchInput):
+    if searchInput in website_lst:
+        indices = [index for index, value in enumerate(website_lst) if value == searchInput]
+        
+        for index in indices:
+            print(f'username: {username_lst[index]}, password: {password_lst[index]}')
+    else:
+        print(f'{searchInput} was not found')
+
+
+
 def viewPassword():
     for i in range(len(password_lst)):
         print(f'{i}. website: {website_lst[i]}, username: {username_lst[i]}, password: {password_lst[i]}')
@@ -19,6 +30,26 @@ def deletePassword(deleteInput):
         username_lst.remove(username_lst[deleteInput])
         password_lst.remove(password_lst[deleteInput])
 
-        print("password successfully deleted")
+        print("Password successfully deleted")
     except IndexError :
         print("Please enter a valid prefix")
+
+def updatePassword(updateInput, updateChoice, newValueInput):
+    try:
+        if updateChoice.lower() == "website":
+            website_lst[updateInput] = newValueInput
+        elif updateChoice.lower() == "username" or updateChoice.lower() == "email" or updateChoice.lower() == "username/email":
+            username_lst[updateInput] = newValueInput
+        elif updateChoice.lower() == "password":
+            password_lst[updateInput] = newValueInput
+        else:
+            print("Please enter valid value")
+            return
+
+        print("Value successfully updated")
+    except IndexError :
+        print("Please enter a valid prefix")
+    
+    
+    
+
